766163463d659f569a56d093d167f025a366374f
Cam Wheatley <cameron_2000@live.co.uk>
2024-01-18 18:33:56 +0000

    add initial form validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-console.log('The start of my American Quiz');
+/* This is the form validation */
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    let passwordValue = document.getElementById('password').value;
+    let passwordValueLength = document.getElementById('password').value.length
+    let confirmPasswordValue = document.getElementById('confirm-password').value;
+    let errorMsg = document.getElementById('error-msg');
+
+    if (passwordValue !== confirmPasswordValue) {
+        console.log('passwords do not match');
+        errorMsg.innerHTML = 'Passwords do not match';
+    } else if (passwordValueLength < 6) {
+        console.log('Password must be at least 6 characters');
+        errorMsg.innerHTML = 'password must be at least 6 characters';
+    } else {
+        form.submit();
+    }
+}
+    
+
+let form = document.getElementById('form');
+form.addEventListener('submit', handleSubmit);
+
