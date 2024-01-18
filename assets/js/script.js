75f144b8b3e3a848bc28082bcd7dc173599a1e4d
Cam Wheatley <cameron_2000@live.co.uk>
2024-01-18 20:05:32 +0000

    add further validation to form

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,35 @@
 function handleSubmit(event) {
     event.preventDefault();
 
+    let usernameLength = document.getElementById('username').value.length;
+    let user = document.getElementById('username').value;
     let passwordValue = document.getElementById('password').value;
     let passwordValueLength = document.getElementById('password').value.length
     let confirmPasswordValue = document.getElementById('confirm-password').value;
-    let errorMsg = document.getElementById('error-msg');
+    let userErrorMsg = document.getElementById('user-msg');
+    let passErrorMsg = document.getElementById('error-msg');
 
-    if (passwordValue !== confirmPasswordValue) {
-        console.log('passwords do not match');
-        errorMsg.innerHTML = 'Passwords do not match';
+    if (usernameLength < 6) {
+        console.log('Username must be at least 6 characters');
+        userErrorMsg.innerHTML = 'Username must be at least 6 characters';
+    } else if (!user.match(/[0-9]/)) {
+        console.log('Username must contain a number');
+        userErrorMsg.innerHTML = 'Username must contain a number';
+    } else if (passwordValue !== confirmPasswordValue) {
+        userErrorMsg.innerHTML = '';
+        console.log('Passwords do not match');
+        passErrorMsg.innerHTML = 'Passwords do not match.';
     } else if (passwordValueLength < 6) {
+        userErrorMsg.innerHTML = '';
         console.log('Password must be at least 6 characters');
-        errorMsg.innerHTML = 'password must be at least 6 characters';
+        passErrorMsg.innerHTML = 'Password must be at least 6 characters.';
+    } else if (passwordValueLength > 20) {
+            console.log('Password must be less than 20 characters');
+            passErrorMsg.innerHTML = 'Password must be less than 20 characters.';
+        } else if (!passwordValue.match(/[0-9]/)) {
+            userErrorMsg.innerHTML = '';
+        console.log('Password must contain a number');
+        passErrorMsg.innerHTML = 'Password must contain a number.';
     } else {
         form.submit();
     }
